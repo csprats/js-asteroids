b481ddd9a15c0a6c67531cc0939e858fa8c3b63d
Carlos Sánches <222044220+csprats@users.noreply.github.com>
2025-07-26 06:03:09 +0200

    Create the functional bullets

--- bullet.js
+++ bullet.js
@@ -1,30 +1,47 @@
 // Bullet class
 export class Bullet {
     constructor(x, y, angle, speed) {
-        this.x = x;
-        this.y = y;
-        this.radius = 2.5;
-        this.angle = angle;
-        this.speed = speed;
+        this.x = x
+        this.y = y
+        this.radius = 2.5
+        this.angle = angle
+        this.speed = speed
         this.velocity = {
             x: Math.cos(this.angle) * this.speed,
             y: Math.sin(this.angle) * this.speed
-        };
-        this.active = true;
+        }
+        this.active = false
     }
 
-    update(deltaTime) {
-		this.x += this.velocity.x * deltaTime;
-		this.y += this.velocity.y * deltaTime;
-
+    update(deltaTime, x, y) {
+		//Check if is active
+		if (this.active)	
+		{
+			//Move
+			this.x += this.velocity.x * deltaTime
+			this.y += this.velocity.y * deltaTime
+		}
     }
 
     draw(ctx) {
-        ctx.save();
-        ctx.fillStyle = '#ff6b35';
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.restore();
+		//Check if is active
+		if (this.active)	{
+			ctx.save()
+			
+			//Draw the bullet
+			ctx.beginPath()
+			ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+			ctx.fillStyle = '#ff6b35'
+			ctx.fill()
+			
+			ctx.restore()
+		}
     }
+    borderCollision(width, height) {
+		//If is outside of the map, desactive the bullet
+		if (this.x < 0 || this.x > width || this.y < 0 || this.y > height) {
+			this.active = false;
+			return true
+		}
+	}
 }
--- main.js
+++ main.js
@@ -13,7 +13,8 @@ canvas.style.margin = 'auto'
 const game = new Game(canvas)
 const inputManager = new InputManager()
 const player = new Player(canvas.width / 2, canvas.height / 2)
-
+let bullet = new Bullet(player.x, player.y, player.angle, 500)
+let shoot = false
 // Main game loop
 function gameLoop() {
   if (!game.running) return
@@ -30,13 +31,26 @@ function gameLoop() {
 
   // Draw game objects
   player.draw(game.getContext())
-
-  const bullet = new Bullet(player.x, player.y, player.angle, 500)
-  bullet.update(game.getDeltaTime())
-  bullet.draw(game.getContext())
-
   game.updateUI()
-
+  
+  //Bullet
+  if (inputManager.isKeyPressed(' ') || bullet.active) {
+	//If is not active, create a new one
+	if (!bullet.active) {
+		bullet = new Bullet(player.x, player.y, player.angle, 500)
+	}
+	//Active it
+	bullet.active = true
+	//Move and draw
+	bullet.update(game.getDeltaTime(), player.x, player.y)
+	bullet.draw(game.getContext())
+	
+	//Check for border collision and if it returns true, disable it
+	if (bullet.borderCollision(game.width, game.height)) {
+		bullet.active = false
+	}
+  }
+  
   // Continue the game loop
   requestAnimationFrame(gameLoop)
 }
